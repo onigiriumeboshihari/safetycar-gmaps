fdefd5c8611251dfadca61a1a42fbe8f2d45b1be
onigiriumeboshihari <koyamada89179@gol.com>
2021-04-10 00:55:18 +0900

    Postモデル単体テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :post do
-    
+    place { Faker::Food.dish }
+    time { Faker::Food.dish }
+    more { Faker::Food.description }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
